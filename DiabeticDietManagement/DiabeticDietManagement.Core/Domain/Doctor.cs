347ec38c263c9b9b7e726b3a9316ad4df321c26f
Krzysztof Strugała <kr.strugala@gmail.com>
2018-08-28 21:32:13 +0200

    Added modes Doctor, Patient (both unfinished)

---
+++ DiabeticDietManagement/DiabeticDietManagement.Core/Domain/Doctor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiabeticDietManagement.Core.Domain
+{
+    public class Doctor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}

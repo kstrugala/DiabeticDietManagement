e5a388c3728c56784c39736125cc65f737b0faf4
Krzysztof Strugała <kr.strugala@gmail.com>
2018-10-01 18:24:00 +0200

    Changed port number to 50000

--- DiabeticDietManagement/DiabeticDietManagement.Api/Program.cs
+++ DiabeticDietManagement/DiabeticDietManagement.Api/Program.cs
@@ -20,6 +20,7 @@ namespace DiabeticDietManagement.Api
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls(urls: "http://localhost:50000")
                 .Build();
     }
 }
